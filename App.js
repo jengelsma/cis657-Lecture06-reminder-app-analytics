85831e9e289cbf4353443b5db0394cdf152378e6
Jonathan Engelsma <jengelsma@gmail.com>
2021-05-28 10:00:40 -0400

    Finished Part C

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import AddReminderScreen from './screens/AddRemindersScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import RemindersScreen from './screens/RemindersScreen';
+import { StatusBar } from 'expo-status-bar';
+import { createStackNavigator } from '@react-navigation/stack';
+
 export default function App() {
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Reminders" component={RemindersScreen} />
+        <Stack.Screen name="AddReminder" component={AddReminderScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/AddRemindersScreen.js
@@ -0,0 +1,37 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+const AddReminderScreen = ({ route, navigation }) => {
+  navigation.setOptions({
+    headerLeft: () => (
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("Reminders");
+        }}
+      >
+        <Text> Save </Text>
+      </TouchableOpacity>
+    ),
+    headerRight: () => (
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("Reminders");
+        }}
+      >
+        <Text> Cancel </Text>
+      </TouchableOpacity>
+    ),
+  });
+
+  return (
+    <View>
+      <Text> Add Reminder Screen </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default AddReminderScreen;

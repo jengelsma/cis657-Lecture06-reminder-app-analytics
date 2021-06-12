9e9e7709e7784a7691967503bbd726a4baed25bf
Jonathan Engelsma <jengelsma@gmail.com>
2021-06-12 17:11:54 -0400

    added FB analytics

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import * as Analytics from "expo-firebase-analytics";
+
 import { StyleSheet, Text, View } from 'react-native';
 
 import AddReminderScreen from './screens/AddRemindersScreen';
@@ -7,10 +9,40 @@ import RemindersScreen from './screens/RemindersScreen';
 import { StatusBar } from 'expo-status-bar';
 import { createStackNavigator } from '@react-navigation/stack';
 
+// Gets the current screen from navigation state
+const getActiveRouteName = state => {
+  const route = state.routes[state.index];
+  if (route.state) {
+    // Dive into nested navigators
+    return getActiveRouteName(route.state);
+  }
+  return route.name;
+};
+
 export default function App() {
+
+  const routeNameRef = React.useRef();
+  const navigationRef = React.useRef();
+
+  React.useEffect(() => {
+    const state = navigationRef.current.getRootState();
+
+    // Save the initial route name
+    routeNameRef.current = getActiveRouteName(state);
+  }, []);
+
   const Stack = createStackNavigator();
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      ref={navigationRef}
+      onStateChange={(state) => {
+        const previousRouteName = routeNameRef.current;
+        const currentRouteName = getActiveRouteName(state);
+        if (previousRouteName !== currentRouteName) {
+          Analytics.setCurrentScreen(currentRouteName, currentRouteName);
+        }
+      }}
+    >
       <Stack.Navigator>
         <Stack.Screen name="Reminders" component={RemindersScreen} />
         <Stack.Screen name="AddReminder" component={AddReminderScreen} />
